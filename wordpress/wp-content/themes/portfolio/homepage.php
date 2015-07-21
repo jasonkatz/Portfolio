6ac57d8a6bdb714a3b32529ea655132a700fb658
Jason Katz <jkatz94@gmail.com>
2015-07-21 18:27:53 -0400

    Started homepage template and fixed error log gitignore

---
+++ wordpress/wp-content/themes/portfolio/homepage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of page
+ * and that other 'pages' on you WordPress site will use a
+ * different template.
+ *
+ * @package portfolio
+ *
+ * Template Name: Homepage
+ */
+
+include("header.php"); ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; // End of post loop ?>
+
+<?php include("footer.php"); ?>

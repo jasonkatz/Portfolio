74d2a628f09fe842197b70b21bf5d2a6cd3f921c
Jason Katz <jason@rubensteintech.com>
2015-07-22 15:15:26 -0400

    Added functions.php and removed extra sidebar items

---
+++ wordpress/wp-content/themes/portfolio/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function remove_admin_menu_items() {
+    $remove_menu_items = array(__('Comments'),__('Posts'));
+    global $menu;
+    end ($menu);
+    while (prev($menu)) {
+        $item = explode(' ', $menu[key($menu)][0]);
+        if (in_array($item[0] != NULL ? $item[0] : "", $remove_menu_items)) {
+            unset($menu[key($menu)]);
+        }
+    }
+}
+
+add_action('admin_menu', 'remove_admin_menu_items');
+
+?>

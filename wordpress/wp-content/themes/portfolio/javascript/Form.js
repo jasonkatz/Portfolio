1500041643a65377802f01a98ddd970149703311
Jason Katz <jkatz94@gmail.com>
2015-08-28 09:44:35 -0400

    Finished form overlay. Moving on to form submission and validation

--- wordpress/wp-content/themes/portfolio/javascript/Form.js
+++ wordpress/wp-content/themes/portfolio/javascript/Form.js
@@ -37,11 +37,15 @@ function initForm() {
         obj.addEventListener("focusin", openFormOverlay);
     });
     overlay_close_button.addEventListener("click", closeFormOverlay);
+    overlay.addEventListener("keydown", closeFormOverlay);
 }
 
 // Initialize global input index to -1, for no input selected
 var active_index = -1;
 function openFormOverlay(e) {
+    // Don't open the overlay unless we are on a touch device
+    if (!Modernizr.touch) return;
+
     // Set the global input index
     active_index = inputs.indexOf(e.target);
 
@@ -55,27 +59,41 @@ function openFormOverlay(e) {
 
     // Set appropriate input and value in overlay
     if (inputs[active_index].tagName == 'INPUT') {
+        // Toggle input visibility
         overlay_textarea.style.display = 'none';
         overlay_input.style.display = 'block';
+        // Set input value
         overlay_input.value = inputs[active_index].value;
     } else if (inputs[active_index].tagName == 'TEXTAREA') {
+        // Toggle textarea visibility
         overlay_input.style.display = 'none';
         overlay_textarea.style.display = 'block';
+        // Set textarea value
         overlay_textarea.value = inputs[active_index].value;
     } else {
+        // Hide both inputs
         overlay_input.style.display = 'none';
         overlay_textarea.style.display = 'none';
     }
 
-    animateOpenOverlay();
+    animateOpenOverlay(active_index);
 }
 
 function closeFormOverlay(e) {
+    // Don't close if a key other than enter or escape was pressed
+    if (e.type == 'keydown' && (e.which != 13 && e.which != 27)) return;
+    // Don't close if textarea is in the overlay, since you should be able to have line-breaks
+    if (e.type == 'keydown' && e.which == 13 && inputs[active_index].tagName == 'TEXTAREA') return;
+
     // Save data to form
     if (inputs[active_index].tagName == 'INPUT') {
         inputs[active_index].value = overlay_input.value;
+        // Make sure to unfocus (blur) the input so that the mobile keyboard goes away
+        overlay_input.blur();
     } else if (inputs[active_index].tagName == 'TEXTAREA') {
         inputs[active_index].value = overlay_textarea.value;
+        // Make sure to unfocus (blur) the textarea so that the mobile keyboard goes away
+        overlay_textarea.blur();
     }
 
     // Reset overlay contents
@@ -83,16 +101,16 @@ function closeFormOverlay(e) {
     overlay_input.value = '';
     overlay_textarea.value = '';
 
+    animateCloseOverlay(active_index);
+
     // Readd event listener to active input, and set the input to inactive
     if (active_index != -1) {
         inputs[active_index].addEventListener("focusin", openFormOverlay);
         active_index = -1;
     }
-
-    animateCloseOverlay();
 }
 
-function animateOpenOverlay() {
+function animateOpenOverlay(input_index) {
     overlay.style.display = "block";
     overlay.style.zIndex = 3;
     Velocity(
@@ -103,11 +121,19 @@ function animateOpenOverlay() {
         , {
             duration: 500
             , easing: 'easeInOutCubic'
+            , complete: function() {
+                // Focus overlay input
+                if (inputs[input_index].tagName == 'INPUT') {
+                    overlay_input.focus();
+                } else if (inputs[input_index].tagName == 'TEXTAREA') {
+                    overlay_textarea.focus();
+                }
+            }
         }
     );
 }
 
-function animateCloseOverlay() {
+function animateCloseOverlay(input_index) {
     Velocity(
         $(overlay)
         , {
@@ -119,6 +145,16 @@ function animateCloseOverlay() {
             , complete: function() {
                 overlay.style.display = "none";
                 overlay.style.zIndex = 0;
+
+                // Scroll to bottom
+                Velocity(
+                    $(inputs[input_index])
+                    , "scroll"
+                    , {
+                        duration: 100
+                        , easing: "easeInOutCubic"
+                    }
+                );
             }
         }
     );
